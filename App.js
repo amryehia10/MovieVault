0820b959b7412356dd9ef730694e860a5d9c28d6
Amr Yehia <amr.yehiaz100@gmail.com>
2024-05-15 23:42:24 +0300

    movies

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import BottomTaps from './navigation/BottomTaps';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from 'react-redux';
+import store from './Redux/store/store'
+import StackNavigation from './navigation/StackNavigation';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Provider store={store}>
+          <NavigationContainer>
+            <QueryClientProvider client={queryClient}>
+              <StackNavigation>
+                  {/* <StatusBar hidden={false}></StatusBar> */}
+              </StackNavigation>
+            </QueryClientProvider>
+          </NavigationContainer>
+      </Provider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
